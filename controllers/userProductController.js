be885e29ad411c6a79c536e33522ac8c0caea9f1
Muhammed Sabit <sabithak2000@gmail.com>
2025-03-01 17:30:25 +0530

    user sorting completed

--- controllers/userProductController.js
+++ controllers/userProductController.js
@@ -4,11 +4,37 @@ const category=require('../models/categoryModel')
 
 const loadShope =async(req,res)=>{
     try {
-        const products = await product.find().limit(8)
 
+        const sortOption = req.query.sort
+        let sort ={}
+        
+        switch (sortOption){
+            case 'priceAsc':
+                sort={salePrice:1}
+                break
+            case 'priceDesc':
+                sort={salePrice:-1}    
+                break
+            case 'nameAsc':
+                sort={name:1    }
+                break
+            case 'nameDesc':
+                sort={name:-1}
+                break
+            case 'nameAsc':
+                sort={createdAt:-1}    
+        }
+
+        let productsQuery= product.find().limit(8).sort(sort)
         .populate('brand')
         .populate('category')
-        res.render('user/shope',{products,brand,category})
+        if (sortOption === 'nameAsc' || sortOption === 'nameDesc') {
+            productsQuery = productsQuery.collation({ locale: 'en', strength: 2 });
+          }
+        const products = await productsQuery
+        const categories = await category.find()
+        const brands= await brand.find()
+        res.render('user/shope',{products,categories,brands})
     } catch (error) {
         console.log(error.message)
     }
@@ -19,11 +45,26 @@ const loadViewProduct = async(req,res)=>{
         const id = req.params.id
 
         const products =await product.findById(id)
-        res.render('user/viewProduct',{products})
+        .populate('brand')
+        .populate('category')
+
+        const relatedProducts=await product.find({
+            category:products.category._id,
+            _id:{$ne:id}
+        })
+        .populate('brand')
+        .populate('category')
+        
+
+
+
+        res.render('user/viewProduct',{products,relatedProducts})
     } catch (error) {
         console.log(error.message)
     }
 }
+
+
 
 module.exports={
     loadShope,
